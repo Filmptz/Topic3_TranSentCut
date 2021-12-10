9fca3c55a37378dae53e0af457cd71a81eafad0e
Filmptz <parichaya35@gmail.com>
2021-12-10 13:05:29 +0700

    update append pickle with check split

--- append_pickle.py
+++ append_pickle.py
@@ -1,37 +1,55 @@
 import pickle
 
-news_positive = ()
+news_positive = []
 
-news_negative = ()
+news_negative = []
 
 positive_path = 'data/version2/positive_trans_news.pickle'
 negative_path = 'data/version2/negative_trans_news.pickle'
 
-with open(positive_path, 'rb') as pos: 
-    data_pickle_pos = pickle.load(pos)
+# CHECK DIMFORMAT
+posError, negError = [], []
+checkPosError, checkNegError = False, False
+for i in news_positive :
+    if(len(i) !=  2) :
+        checkPosError = True
+        posError.append(i)
+for i in news_negative :
+    if(len(i) !=  2) :
+        checkNegError = True
+        negError.append(i)
+if(checkPosError or checkNegError) :
+    print('------- Positive ERROR ------- ')
+    print('-none-') if len(posError) == 0 else print(posError)
+    print('------- Negative ERROR ------- ')
+    print('-none-') if len(negError) == 0 else print(negError)
+else :
+    with open(positive_path, 'rb') as pos: 
+        data_pickle_pos = pickle.load(pos)
 
-with open(negative_path, 'rb') as neg: 
-    data_pickle_neg = pickle.load(neg)
+    with open(negative_path, 'rb') as neg: 
+        data_pickle_neg = pickle.load(neg)
 
-for npos in news_positive:
-    data_pickle_pos.append(npos)
+    for npos in news_positive:
+        data_pickle_pos.append(npos)
 
-for nneg in news_negative:
-    data_pickle_neg.append(nneg)
+    for nneg in news_negative:
+        data_pickle_neg.append(nneg)
 
-with open(positive_path, 'wb') as posw: 
-    pickle.dump(data_pickle_pos, posw)
+    with open(positive_path, 'wb') as posw: 
+        pickle.dump(data_pickle_pos, posw)
 
-with open(negative_path, 'wb') as negw:
-    pickle.dump(data_pickle_neg, negw)
+    with open(negative_path, 'wb') as negw:
+        pickle.dump(data_pickle_neg, negw)
 
-with open(positive_path, 'rb') as pos: 
-    data_pickle_pos_new = pickle.load(pos)
+    with open(positive_path, 'rb') as pos: 
+        data_pickle_pos_new = pickle.load(pos)
 
-with open(negative_path, 'rb') as neg: 
-    data_pickle_neg_new = pickle.load(neg)
+    with open(negative_path, 'rb') as neg: 
+        data_pickle_neg_new = pickle.load(neg)
 
-print('POSITIVE LAST INDEX AFTER APPEND')
-print(data_pickle_pos_new[-1])
-print('NEGATIVE LAST INDEX AFTER APPEND')
-print(data_pickle_neg_new[-1])
+    print('----- POSITIVE LAST INDEX AFTER APPEND -----')
+    print(data_pickle_pos_new[-1])
+    print('----- NEGATIVE LAST INDEX AFTER APPEND -----')
+    print(data_pickle_neg_new[-1])
+
